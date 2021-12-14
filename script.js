2290bfc62928511cd492cc8c8d1fddabeaa2382e
tara <tara.trbos@gmail.com>
2021-12-14 16:47:55 +0100

    added stuff

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
+const dice = document.querySelectorAll(".dice");
+
 function diceRoll() {
-    const roll = Math.floor(Math.random() * 6 + 1);
-    return roll;
+  const roll = Math.floor(Math.random() * 6 + 1);
+  return roll;
 }
 
 function diceDisplay() {
-    const dice = document.querySelectorAll(".dice");
-    Array.from(dice).forEach((die) => {
-        die.innerHTML = diceRoll();
-    });
+  Array.from(dice).forEach((die) => {
+    die.innerHTML = diceRoll();
+  });
 }
-
+function stopDice() {
+  Array.from(dice).forEach(function (die) {
+    die.addEventListener("click", () => die.innerHTML = ); //how would you stop the clicked die?
+  });
+}
 function round() {
-    const button1 = document.querySelector("#button1");
-    let rolls = 3;
+  const button1 = document.querySelector("#button1");
+  let rolls = 3;
 
-    button1.addEventListener("click", () => { diceDisplay()
-      if (!--rolls) button1.setAttribute("disabled", "true");
-    });
+  button1.addEventListener("click", () => {
+    diceDisplay();
+    if (!--rolls) button1.setAttribute("disabled", "true");
+    else {
+      stopDice();
+      getDice();
+      ones();
+    }
+  });
 }
+
+function getDice() {
+  return [...dice].map(function (die) {
+    return die.textContent;
+  });
+}
+function ones() {
+  if (getDice().includes(1) == true) {
+    console.log("it works");
+  }
+}
+
 round();
